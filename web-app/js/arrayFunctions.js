330c5ba4ec8b96a7a9889970eb395c70fdcb7282
Pankaj K S <SPankaj@ESTUATE.COM>
2019-09-17 17:38:18 +0530

    Updated till the map, filter, forEach functions usages

---
+++ web-app/js/arrayFunctions.js
@@ -0,0 +1,78 @@
+// forEach function:
+
+let numArray = [1,2,3,4,5];
+
+let res  = numArray.map((num, index, arr) => {
+	return arr;
+});
+console.log("Res --> ", num);
+
+let objectArray = [
+	{name: "Pavithra",id: 5100,phone: 9819759230},
+	{name: "Ramesh",id: 5101,phone: 9819759231},
+	{name: "Ranjitha",id: 5102,phone: 9819759232},
+	{name: null,id: 5130,phone: 9819759234},
+	{name: "Shiva",id: 5103,phone: 9819759233},
+	{name: "Pankaj",id: 5132,phone: 9819759234},
+];
+
+
+// forEach function()
+console.log("\n ============== forEach() ======================");
+let forResult = objectArray.forEach((ele) => {
+	console.log(ele);
+	return "abc"
+});
+console.log("Return type of the forEach function is : ", forResult);
+
+// map function()
+console.log("\n ============== map() ======================");
+
+let mapResult = objectArray.map((ele, index) => {
+	return {...ele};
+});
+
+// mapResult[0].name = "Aish";
+console.log("Original objectArray : ", objectArray);
+console.log("Return type of the map function is : ", mapResult);
+
+// filter function
+console.log("\n ==================== filter() ======================");
+let filterResult = objectArray.filter((ele) => {
+	return ele.name == null;  // returns the ele if the name on the ele object is null
+});
+
+console.log("Original objectArray : ", objectArray);
+console.log("Return type of the filter function is : ", filterResult);
+
+// every function - All the "ele" must pass the condition mentioned in the
+// callback function in order to get the return value as true.
+
+console.log("\n ==================== every() =====================");
+let everyResult = objectArray.every((ele) => {
+	console.log(ele);
+	return ele.name !== null;
+});
+
+console.log("Original objectArray : ", objectArray);
+console.log("Return type of the every function is : ", everyResult);
+
+
+// some function - If any one of the "ele" pass the condition mentioned inside the
+// callback function then the method's return value will be true.
+console.log("\n ==================== some() ======================");
+let someResult = objectArray.some((ele) => {
+	if(ele.id == 5102){
+		return true;   // some breaks and comes out of the loop whenever it encounters true
+	}
+	return false;
+});
+
+console.log("Original objectArray : ", objectArray);
+console.log("Return type of the some function is : ", someResult);
+
+
+// Reverse method reverse the array on which it is called. Content will be reversed.
+// let rev = [...objectArray];
+// rev.reverse();
+// console.log(rev);
--- web-app/js/roughPractice.js
+++ web-app/js/roughPractice.js
@@ -1,29 +1,31 @@
-// document.designMode = "on";
-// "use strict"
+// // document.designMode = "on";
+// // "use strict"
 
-let arr = ["tony", "stark", "avengers"];
-let obj = { name: "Stark", sal: 3200898, role: "Scientist", currency: "USD" };
+// let arr = ["tony", "stark", "avengers"];
+// let obj = { name: "Stark", sal: 3200898, role: "Scientist", currency: "USD" };
 
-// For loop: only for arrays
-for(let i=0; i < arr.length ; i++) {
-	console.log(arr[i]);
-}
+// // For loop: only for arrays
+// for(let i=0; i < arr.length ; i++) {
+// 	console.log(arr[i]);
+// }
 
-// For in loop for both the Object and Array
-// for..in loop in case of an Object
-for(let key in obj) {
-	console.log(`key is ${key} and value is ${obj[key]}`);
-}
-// for..in loop in case of an Array
-for(let index in obj) {
-	console.log(`Index is ${index} and value is ${arr[index]}`);
-}
+// // For in loop for both the Object and Array
+// // for..in loop in case of an Object
+// for(let key in obj) {
+// 	console.log(`key is ${key} and value is ${obj[key]}`);
+// }
+// // for..in loop in case of an Array
+// for(let index in obj) {
+// 	console.log(`Index is ${index} and value is ${arr[index]}`);
+// }
 
 
-// For..of loop on Array only. Not applicable on the object
-for(let element of arr) {
-	console.log(element);
-}
+// // For..of loop on Array only. Not applicable on the object
+// for(let element of arr) {
+// 	console.log(element);
+// }
+
+
 
 
 // // Prime number solution.
@@ -46,3 +48,19 @@ for(let element of arr) {
 // 	}
 // }
 // console.log(count);
+
+
+// ==========>
+var kvArray = [{key: 1, value: 10}, {key: 2, value: 20}, {key: 3, value: 30}];
+
+var arr = kvArray.map((ele) => {
+	return {...ele};
+});
+
+console.log(kvArray);
+console.log(arr);
+
+arr[2].key = 45; 
+
+console.log(kvArray);
+console.log(arr);
